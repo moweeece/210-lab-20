19c107a600789d4aac6d258084a47b1452ecb516
moweeece <mjespinosa990@gmail.com>
2024-10-09 22:04:13 -0700

    Third Commit

--- main.cpp
+++ main.cpp
@@ -76,23 +76,19 @@ int main() {
     delete chairPtr;
 
     //creating dynamic chair object with constructor
-    Chair *livingChair = new Chair(3);
-    livingChair->setPrices(525.25, 434.34, 252.52);
+    double livingPrices[SIZE] = {525.25, 434.34, 252.52};
+    Chair *livingChair = new Chair(3, livingPrices);
     livingChair->print();
     delete livingChair;
     livingChair = nullptr;
 
     //creating dynamic array of chair objects
     Chair *collection = new Chair[SIZE];
-    collection[0].setLegs(4);
-    collection[0].setPrices(441.41, 552.52, 663.63);
-    collection[1].setLegs(4);
-    collection[1].setPrices(484.84, 959.59, 868.68);
-    collection[2].setLegs(4);
-    collection[2].setPrices(626.26, 515.15, 757.57);
 
     for (int i = 0; i < SIZE; i++)
         collection[i].print();
+
+    delete[] collection;
 
     return 0;
 }
